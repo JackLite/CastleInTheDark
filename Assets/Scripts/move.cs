532d0c83705a06e33c04218bcf1b503dfc2b622c
JackLite <jackkdlite@gmail.com>
2017-02-27 23:02:06 +0300

    Удалил лишнее, оставил только персонажа. Добавил первый этаж замка на обычной плоскости. Также доработал перемещение + поворот. Начал делать прыжок. Пока он прыгает, но в прыжке можно прыгнуть ещё раз и так до бесконечности.

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -4,7 +4,8 @@ using System;
 
 public class move : MonoBehaviour {
 
-    private float m_MovementInputValue;
+    private float m_MovementForBackInputValue;
+    private float m_MovementLeftRightInputValue;
     private float m_MouseXInputValue;
     private float m_MouseYInputValue;
     private float curXRotation;
@@ -22,7 +23,11 @@ public class move : MonoBehaviour {
 	void Update () {
         Move();
         MouseRotate();
-
+        if(Input.GetButtonDown("Jump"))
+        {
+            Rigidbody rig = GetComponent<Rigidbody>();
+            rig.AddForce(0, 500, 0);
+        }
     }
 
     private void FixedUpdate()
@@ -32,19 +37,20 @@ public class move : MonoBehaviour {
 
     void Move ()
     {
-        m_MovementInputValue = Input.GetAxis("Vertical");
-        
+        m_MovementForBackInputValue = Input.GetAxis("Vertical");
+        m_MovementLeftRightInputValue = Input.GetAxis("Horizontal");
         Rigidbody rig = GetComponent<Rigidbody>();
-        Vector3 newPosition = transform.forward * m_MovementInputValue * Time.deltaTime;
-        rig.MovePosition(rig.position + newPosition);
+        Vector3 newForwardPosition = transform.forward * m_MovementForBackInputValue * Time.deltaTime * 5;
+        Vector3 newRightPosition = transform.right * m_MovementLeftRightInputValue * Time.deltaTime * 5;
+        rig.MovePosition(rig.position + newForwardPosition + newRightPosition);
     } 
 
     void MouseRotate()
     {
         m_MouseXInputValue = Input.GetAxis("Mouse Y") * Time.deltaTime;
         m_MouseYInputValue = Input.GetAxis("Mouse X") * Time.deltaTime;
-        curXRotation += m_MouseXInputValue * 20;
-        curYRotation += m_MouseYInputValue * 20;
+        curXRotation += m_MouseXInputValue * 200;
+        curYRotation += m_MouseYInputValue * 200;
         Rigidbody rig = GetComponent<Rigidbody>();
         rig.transform.rotation = Quaternion.Euler(0, curYRotation, 0);
         mainCamera.transform.localRotation = Quaternion.Euler(-curXRotation, 0, 0);
