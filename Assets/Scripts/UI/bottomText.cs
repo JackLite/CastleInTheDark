f1fddc44a6c5b4ac8a593118c41fcbc21cb1f9c9
JackLite <jackkdlite@gmail.com>
2017-05-21 10:52:13 +0300

    Подчистил от мусора и добавил текстовый гуи

---
+++ Assets/Scripts/UI/bottomText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bottomText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void changeText(string newText)
+    {
+        Text text = GetComponent<Text>();
+        text.text = newText;
+    }
+}
