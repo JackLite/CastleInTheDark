1f148b1d4cf93da16e6579bf8ce609931e27d880
JackLite <jackkdlite@gmail.com>
2017-08-20 11:27:50 +0300

    Переделал скрипты взаимодействия с объектами. Добавл два соответствующих интерфейса IInteractivable для всех интерактивных объектов и IActionable (наследуется от IInteractivable) для объектов с которыми можно взаимодействовать (например, дверь). Аналогично добавим в дальнейшем интерфейс ITakable для объектов, которые можно положить в инвентарь.

---
+++ Assets/UIController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject bottomText;
+    [SerializeField]
+    GameObject mainIcon;
+	
+    public void changeText(string newText)
+    {
+        bottomText.GetComponent<Text>().text = newText;
+    }
+}
